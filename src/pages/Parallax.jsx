5286ed6cdb7afe66ab51272d21f573fe026006e9
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-05-21 02:12:52 +0200

    Making intro for paralllax website

---
+++ src/pages/Parallax.jsx
@@ -0,0 +1,12 @@
+import Intro from "../components/Intro";
+
+function Parallax() {
+	return (
+		<div>
+			<Intro />
+			<div className="h-[100vh] bg-white">Scroll down to see parallax</div>
+		</div>
+	);
+}
+
+export default Parallax;
