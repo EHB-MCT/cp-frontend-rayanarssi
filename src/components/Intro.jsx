ecb22fdfba853d9cce8cc462c8e59f3891896d0f
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-05-21 16:00:09 +0200

    styling and z-index management for parallax layers

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -45,8 +45,10 @@ const Intro = () => {
 						key={index}
 						src={path}
 						alt={img}
-						className="parallax-layer absolute w-full object-cover top-0 left-0"
-						style={{ zIndex: index }}
+						className={`parallax-layer absolute 
+              ${isText ? "w-3/4 left-1/2 transform -translate-x-1/2" : "w-full"}
+              top-0 object-contain`}
+						style={{ zIndex: 10 + index, pointerEvents: "none", opacity: 1 }}
 					/>
 				);
 			})}
