91da49c3ce4451ad5b5aaa6a70c08bb3669996d4
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-05-22 20:28:13 +0200

    Add scroll detection and tentacle animations to Scene3 component

--- src/components/Scene3.jsx
+++ src/components/Scene3.jsx
@@ -1,13 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 const Scene3 = () => {
 	const [scroll, setScrollY] = useState(0);
+	const [hasEntered, setHasEntered] = useState(false);
+	const [showTentacles, setShowTentacles] = useState(false);
+	const sceneRef = useRef(null);
 	const base = import.meta.env.BASE_URL;
 
 	useEffect(() => {
 		const handleScroll = () => {
 			setScrollY(window.scrollY);
+			if (sceneRef.current) {
+				const top = sceneRef.current.offsetTop;
+				if (window.scrollY + window.innerHeight * 0.5 >= top) {
+					setHasEntered(true);
+				}
+			}
 		};
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
@@ -16,6 +25,7 @@ const Scene3 = () => {
 	return (
 		<div
 			id="Scene3"
+			ref={sceneRef}
 			style={{
 				width: "100vw",
 				height: "100vh",
@@ -33,7 +43,7 @@ const Scene3 = () => {
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "center",
-					fontSize: "36px",
+					fontSize: "34px",
 					textAlign: "center",
 					fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
 				}}
@@ -47,7 +57,6 @@ const Scene3 = () => {
 					She would get human legs but lose her <br /> voice. If the prince did
 					not love her, she would perish.
 				</p>
-
 				{/* Top */}
 				<motion.div
 					className="Rock_background"
@@ -64,10 +73,12 @@ const Scene3 = () => {
 						transform: `translateY(${scrollY * 0.1}px)`,
 					}}
 				/>
-
 				{/* Rock left */}
 				<motion.div
 					className="Rock"
+					initial={{ x: "-100vw" }}
+					animate={hasEntered ? { x: 0 } : {}}
+					transition={{ duration: 1 }}
 					style={{
 						position: "absolute",
 						top: "50%",
@@ -80,10 +91,12 @@ const Scene3 = () => {
 						zIndex: 6,
 					}}
 				/>
-
 				{/* Rock right */}
 				<motion.div
 					className="Rock"
+					initial={{ x: "100vw" }}
+					animate={hasEntered ? { x: 0 } : {}}
+					transition={{ duration: 1 }}
 					style={{
 						position: "absolute",
 						top: "47%",
@@ -96,10 +109,12 @@ const Scene3 = () => {
 						zIndex: 6,
 					}}
 				/>
-
 				{/* Potion */}
 				<motion.div
 					className="Potion"
+					initial={{ y: 200, opacity: 0 }}
+					animate={hasEntered ? { y: 0, opacity: 1 } : {}}
+					transition={{ duration: 1.2 }}
 					style={{
 						position: "absolute",
 						top: "40%",
@@ -113,62 +128,55 @@ const Scene3 = () => {
 						zIndex: 6,
 					}}
 					onMouseEnter={() => {
+						setShowTentacles(true);
 						const audio = new Audio(`${base}/assets/Witch_sound.mp3`);
 						audio.play();
 					}}
-				/>
-
-				{/* Potion */}
-				<motion.div
-					className="Potion"
-					style={{
-						position: "absolute",
-						top: "40%",
-						left: "46%",
-						width: "30vw",
-						height: "30vh",
-						backgroundImage: `url(${base}/assets/Potion.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 6,
-					}}
-					onMouseEnter={() => {
-						const audio = new Audio(`${base}/assets/Witch_sound.mp3`);
-						audio.play();
+					onMouseLeave={() => {
+						setShowTentacles(false);
 					}}
 				/>
-
 				{/* Tentacle left */}
-				<motion.div
-					className="Tentacle_left"
-					style={{
-						position: "absolute",
-						top: "40%",
-						left: "18%",
-						width: "40vw",
-						height: "30vh",
-						backgroundImage: `url(${base}/assets/Tentacle_left.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 0,
-					}}
-				/>
+				{showTentacles && (
+					<motion.div
+						className="Tentacle_left"
+						initial={{ opacity: 0, y: 50 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.5 }}
+						style={{
+							position: "absolute",
+							top: "40%",
+							left: "18%",
+							width: "40vw",
+							height: "30vh",
+							backgroundImage: `url(${base}/assets/Tentacle_left.png)`,
+							backgroundSize: "contain",
+							backgroundRepeat: "no-repeat",
+							zIndex: 0,
+						}}
+					/>
+				)}
 
 				{/* Tentacle right */}
-				<motion.div
-					className="Tentacle_right"
-					style={{
-						position: "absolute",
-						top: "30%",
-						left: "60%",
-						width: "80vw",
-						height: "60vh",
-						backgroundImage: `url(${base}/assets/Tentacle_right.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 0,
-					}}
-				/>
+				{showTentacles && (
+					<motion.div
+						className="Tentacle_right"
+						initial={{ opacity: 0, y: 50 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.5 }}
+						style={{
+							position: "absolute",
+							top: "30%",
+							left: "60%",
+							width: "80vw",
+							height: "60vh",
+							backgroundImage: `url(${base}/assets/Tentacle_right.png)`,
+							backgroundSize: "contain",
+							backgroundRepeat: "no-repeat",
+							zIndex: 0,
+						}}
+					/>
+				)}
 			</div>
 		</div>
 	);
