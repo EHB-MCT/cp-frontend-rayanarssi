05eb9bc37ca9e3429c56d127fdb6196919a869eb
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-05-22 12:44:47 +0200

    Add static scene3 for my parallaw website

--- src/pages/Parallax.jsx
+++ src/pages/Parallax.jsx
@@ -1,14 +1,15 @@
 import Intro from "../components/Intro";
 import Scene1 from "../components/Scene1";
 import Scene2 from "../components/Scene2";
+import Scene3 from "../components/Scene3";
 
 function Parallax() {
 	return (
 		<div style={{ overflowX: "hidden" }}>
 			<Intro />
 			<Scene1 />
-			<Scene2 /> 
-		
+			<Scene2 />
+			<Scene3 />
 		</div>
 	);
 }
