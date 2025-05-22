02940112619d53d713fcab1e540fa29bf344c1f6
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-05-22 11:36:22 +0200

    Add static Scene2 to my parallaw website

--- src/components/Scene2.jsx
+++ src/components/Scene2.jsx
@@ -28,104 +28,136 @@ const Scene1 = () => {
 					width: "100vw",
 					height: "100vh",
 					position: "relative",
-					backgroundColor: "#302E79",
+					background: "linear-gradient(#302E79, #1A0F4A)",
 					color: "white",
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "center",
-					fontSize: "68px",
+					fontSize: "48px",
 					textAlign: "center",
 					fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
 				}}
 			>
 				<p>
-					Deep in the ocean, a <br /> young mermaid dreamed <br />
-					of the human world.
+					One stormy night, she rescued a <br /> drowning prince, bringing him
+					to <br /> shore before vanishing into the sea.
 				</p>
 
-				{/* Mermaid */}
+				{/* Ground */}
 				<motion.div
-					className="Mermaid"
+					className="Ground"
 					style={{
 						position: "absolute",
-						top: "18%",
-						right: "-50vw",
-						width: "80vw",
+						bottom: "-15vw",
+						left: "50%",
+						width: "100vw",
+						height: "65vh",
+						backgroundImage: `url(${base}/assets/Ground.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 1,
+						transform: `translate(-50%, ${scrollY * 0.1}px)`,
+					}}
+				/>
+
+				{/* Boat */}
+				<motion.div
+					className="Boat"
+					style={{
+						position: "absolute",
+						top: "32%",
+						right: "-45vw",
+						width: "70vw",
 						height: "70vh",
-						backgroundImage: `url(${base}/assets/Mermaid.png)`,
+						backgroundImage: `url(${base}/assets/Boat.png)`,
 						backgroundSize: "contain",
 						backgroundRepeat: "no-repeat",
 						zIndex: 5,
 					}}
 				/>
 
-				{/* Rock */}
+				{/* Sinking prince */}
 				<motion.div
-					className="Rock"
-					style={{
-						position: "absolute",
-						top: "50%",
-						left: "-2vw",
-						width: "60vw",
-						height: "55vh",
-						backgroundImage: `url(${base}/assets/Rock_left.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 6,
-					}}
-				/>
-
-				{/* Pink Coral */}
-				<motion.div
-					className="Pink_Coral"
-					style={{
-						position: "absolute",
-						top: "65%",
-						left: "18vw",
-						width: "40vw",
-						height: "35vh",
-						backgroundImage: `url(${base}/assets/Pink_coral.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 6,
-					}}
-				/>
-
-				{/* Green Coral */}
-				<motion.div
-					className="Green_Coral"
-					style={{
-						position: "absolute",
-						top: "55%",
-						left: "10vw",
-						width: "30vw",
-						height: "25vh",
-						backgroundImage: `url(${base}/assets/Green_coral.png)`,
-						backgroundSize: "contain",
-						backgroundRepeat: "no-repeat",
-						zIndex: 5,
-					}}
-				/>
-
-				{/* Red Coral */}
-				<motion.div
-					className="Red_Coral"
+					className="Sinking_prince"
 					style={{
 						position: "absolute",
 						top: "30%",
-						left: "3vw",
-						width: "40vw",
-						height: "35vh",
-						backgroundImage: `url(${base}/assets/Red_coral.png)`,
+						left: "5vw",
+						width: "60vw",
+						height: "55vh",
+						backgroundImage: `url(${base}/assets/Sinking_prince.png)`,
 						backgroundSize: "contain",
 						backgroundRepeat: "no-repeat",
-						zIndex: 5,
+						zIndex: 6,
 					}}
 				/>
 
-				{/* Blue fish 1 */}
+				{/* Bubble  */}
 				<motion.div
-					className="Blue_Fish_1"
+					className="Bubble"
+					style={{
+						position: "absolute",
+						top: "20%",
+						left: "13vw",
+						width: "10vw",
+						height: "5vh",
+						backgroundImage: `url(${base}/assets/Bubble.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 8,
+					}}
+				/>
+				{/* Bubble  */}
+				<motion.div
+					className="Bubble"
+					style={{
+						position: "absolute",
+						top: "25%",
+						left: "15vw",
+						width: "8vw",
+						height: "4vh",
+						backgroundImage: `url(${base}/assets/Bubble.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 8,
+					}}
+				/>
+
+				{/* Bubble  */}
+				<motion.div
+					className="Bubble"
+					style={{
+						position: "absolute",
+						top: "30%",
+						left: "14vw",
+						width: "6vw",
+						height: "3vh",
+						backgroundImage: `url(${base}/assets/Bubble.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 8,
+					}}
+				/>
+
+				{/* Bubble  */}
+				<motion.div
+					className="Bubble"
+					style={{
+						position: "absolute",
+						top: "34%",
+						left: "14vw",
+						width: "4vw",
+						height: "2vh",
+						backgroundImage: `url(${base}/assets/Bubble.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 8,
+					}}
+				/>
+
+				{/* Blue fish */}
+				<motion.div
+					className="Blue_Fish"
 					style={{
 						position: "absolute",
 						top: "75%",
@@ -139,9 +171,9 @@ const Scene1 = () => {
 					}}
 				/>
 
-				{/* Blue fish  */}
+                {/* Blue fish */}
 				<motion.div
-					className="Blue_Fish_2"
+					className="Blue_Fish"
 					style={{
 						position: "absolute",
 						top: "82%",
@@ -155,13 +187,30 @@ const Scene1 = () => {
 					}}
 				/>
 
+				{/* Blue fish  */}
+				<motion.div
+					className="Blue_Fish_2"
+					style={{
+						position: "absolute",
+						top: "70%",
+						left: "55vw",
+						width: "12vw",
+						height: "7vh",
+						backgroundImage: `url(${base}/assets/Blue_fish.png)`,
+						backgroundSize: "contain",
+						backgroundRepeat: "no-repeat",
+						zIndex: 8,
+					}}
+				/>
+
+                
 				{/* Purple fish  */}
 				<motion.div
 					className="Purple_Fish"
 					style={{
 						position: "absolute",
 						top: "15%",
-						left: "20vw",
+						left: "70vw",
 						width: "15vw",
 						height: "10vh",
 						backgroundImage: `url(${base}/assets/Purple_fish.png)`,
@@ -171,17 +220,16 @@ const Scene1 = () => {
 					}}
 				/>
 
-               
-                {/* Bubble  */}
+                {/* Purple fish  */}
 				<motion.div
-					className="Bubble"
+					className="Purple_Fish"
 					style={{
 						position: "absolute",
-						top: "45%",
-						left: "27vw",
-						width: "6vw",
-						height: "3vh",
-						backgroundImage: `url(${base}/assets/Bubble.png)`,
+						top: "25%",
+						left: "75vw",
+						width: "12vw",
+						height: "7vh",
+						backgroundImage: `url(${base}/assets/Purple_fish.png)`,
 						backgroundSize: "contain",
 						backgroundRepeat: "no-repeat",
 						zIndex: 8,
--- src/pages/Parallax.jsx
+++ src/pages/Parallax.jsx
@@ -1,11 +1,14 @@
 import Intro from "../components/Intro";
 import Scene1 from "../components/Scene1";
+import Scene2 from "../components/Scene2";
 
 function Parallax() {
 	return (
 		<div style={{ overflowX: "hidden" }}>
 			<Intro />
 			<Scene1 />
+			<Scene2 /> 
+		
 		</div>
 	);
 }
